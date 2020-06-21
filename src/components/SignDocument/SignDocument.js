e5918f31a017480bf5bd76b020f74683234a27a4
Andrey <a.so777@live.ca>
2020-06-21 10:15:07 -0700

    Toggling Fields on/off based on the email of the signer

--- src/components/SignDocument/SignDocument.js
+++ src/components/SignDocument/SignDocument.js
@@ -1,14 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import {
-  Box,
-  Column,
-  Heading,
-  Row,
-  Stack,
-  Text,
-  Button,
-} from 'gestalt';
+import { Box, Column, Heading, Row, Stack, Text, Button } from 'gestalt';
 import { selectDocToSign } from './SignDocumentSlice';
 import { storage } from '../Firebase/firebase';
 import { selectUser } from '../Firebase/firebaseSlice';
@@ -18,7 +10,7 @@ import './SignDocument.css';
 
 const SignDocument = () => {
   const [instance, setInstance] = useState(null);
-  
+
   const docRef = useSelector(selectDocToSign);
   const user = useSelector(selectUser);
   const { uid, email } = user;
@@ -40,17 +32,53 @@ const SignDocument = () => {
           'signatureToolButton',
           'eraserToolButton',
           'stickyToolButton',
+          'freeTextToolButton',
           'miscToolGroupButton',
         ],
       },
       viewer.current,
-    ).then( async instance => {
-      const { docViewer } = instance;
+    ).then(async instance => {
+      const { docViewer, annotManager, Annotations } = instance;
       setInstance(instance);
+
+      // load document
       const storageRef = storage.ref();
-      console.log(docRef);
       const URL = await storageRef.child(docRef).getDownloadURL();
       instance.docViewer.loadDocument(URL);
+
+      const normalStyles = (widget) => {
+        if (widget instanceof Annotations.TextWidgetAnnotation) {
+          return {
+            'background-color': '#a5c7ff',
+            color: 'white',
+            'font-size': '20px',
+          };
+        } else if (widget instanceof Annotations.SignatureWidgetAnnotation) {
+          return {
+            border: '1px solid #a5c7ff',
+          };
+        }
+      };
+
+      annotManager.on('annotationChanged', (annotations, action, { imported }) => {
+        // set fields to readonly if they are not related to the user
+        if (imported && action === 'add') {
+          annotations.forEach(function(annot) {
+            if (annot instanceof Annotations.WidgetAnnotation) {
+              console.log(annot);
+              if (!annot.fieldName.startsWith(email)) {
+                annot.fieldFlags.set('ReadOnly', true);
+                Annotations.WidgetAnnotation.getCustomStyles = normalStyles;
+              }
+            }
+          });
+        }
+      });
+
+      docViewer.on('annotationsLoaded', () => {
+        // customize styles of the form field
+        
+      });
     });
   }, []);
 
@@ -65,12 +93,24 @@ const SignDocument = () => {
             <Row gap={1}>
               <Stack>
                 <Box padding={2}>
-                  <Text>{'Complete signing'}</Text>
+                  <Button
+                    onClick={() => {}}
+                    accessibilityLabel="next field"
+                    text="Next field"
+                    iconEnd="arrow-forward"
+                  />
                 </Box>
                 <Box padding={2}>
                   <Button
-                    onClick={() => {
-                    }}
+                    onClick={() => {}}
+                    accessibilityLabel="Previous field"
+                    text="Previous field"
+                    iconEnd="arrow-back"
+                  />
+                </Box>
+                <Box padding={2}>
+                  <Button
+                    onClick={() => {}}
                     accessibilityLabel="complete signing"
                     text="Complete signing"
                     iconEnd="compose"
