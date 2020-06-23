8f1e867c31c6fb5dfc8b64031e236f6175994f1d
Andrey <a.so777@live.ca>
2020-06-22 20:09:31 -0700

    Implemented spinners to wait for Firebase

--- src/components/Lists/SignList.js
+++ src/components/Lists/SignList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table, Text } from 'gestalt';
+import { Button, Table, Text, Spinner } from 'gestalt';
 import 'gestalt/dist/gestalt.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchForDocumentToSign } from '../Firebase/firebase';
@@ -12,54 +12,61 @@ const SignList = () => {
   const { email } = user;
 
   const [docs, setDocs] = useState([]);
-  
-  const dispatch = useDispatch(); 
+  const [show, setShow] = useState(true);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-      async function getDocs() {
-        const docsToSign = await searchForDocumentToSign(email);
-        console.log(docsToSign);
-        setDocs(docsToSign);
-      }
-    
-      getDocs();
+    async function getDocs() {
+      const docsToSign = await searchForDocumentToSign(email);
+      setDocs(docsToSign);
+      setShow(false);
+    }
+
+    setTimeout(getDocs, 3000);
   }, []);
 
   return (
     <div>
-      {docs.length > 0 ? (
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>
-                <Text weight="bold">From</Text>
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {docs.map(doc => (
-              <Table.Row key={doc.docRef}>
-                <Table.Cell>
-                  <Text>{doc.email}</Text>
-                </Table.Cell>
-                <Table.Cell>
-                  <Button
-                    onClick={event => {
-                      const { docRef, docId } = doc;
-                      dispatch(setDocToSign({ docRef, docId }));
-                      navigate(`/signDocument`);
-                    }}
-                    text="Sign"
-                    color="blue"
-                    inline
-                  />
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+      {show ? (
+        <Spinner show={show} accessibilityLabel="spinner" />
       ) : (
-        'You do not have any documents to sign'
+        <div>
+          {docs.length > 0 ? (
+            <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>
+                    <Text weight="bold">From</Text>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {docs.map(doc => (
+                  <Table.Row key={doc.docRef}>
+                    <Table.Cell>
+                      <Text>{doc.email}</Text>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        onClick={event => {
+                          const { docRef, docId } = doc;
+                          dispatch(setDocToSign({ docRef, docId }));
+                          navigate(`/signDocument`);
+                        }}
+                        text="Sign"
+                        color="blue"
+                        inline
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          ) : (
+            'You do not have any documents to sign'
+          )}
+        </div>
       )}
     </div>
   );
--- src/components/Lists/SignedList.js
+++ src/components/Lists/SignedList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table, Text } from 'gestalt';
+import { Button, Table, Text, Spinner } from 'gestalt';
 import 'gestalt/dist/gestalt.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { searchForDocumentsSigned } from '../Firebase/firebase';
@@ -11,56 +11,60 @@ const SignedList = () => {
   const user = useSelector(selectUser);
   const { email } = user;
   const [docs, setDocs] = useState([]);
-  
-  const dispatch = useDispatch(); 
+  const [show, setShow] = useState(true);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-      async function getDocs() {
-        const docsToView = await searchForDocumentsSigned(email);
-        console.log(docsToView);
-        setDocs(docsToView);
-      }
-    
-      getDocs();
+    async function getDocs() {
+      const docsToView = await searchForDocumentsSigned(email);
+      setDocs(docsToView);
+      setShow(false);
+    }
+    setTimeout(getDocs, 3000);
   }, []);
-  
-
 
   return (
     <div>
-      {docs.length > 0 ? (
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>
-                <Text weight="bold">From</Text>
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {docs.map(doc => (
-              <Table.Row key={doc.docRef}>
-                <Table.Cell>
-                  <Text>{doc.emails}</Text>
-                </Table.Cell>
-                <Table.Cell>
-                  <Button
-                    onClick={event => {
-                      const { docRef, docId } = doc;
-                      dispatch(setDocToView({ docRef, docId }));
-                      navigate(`/viewDocument`);
-                    }}
-                    text="View"
-                    color="blue"
-                    inline
-                  />
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+      {show ? (
+        <Spinner show={show} accessibilityLabel="spinner" />
       ) : (
-        'You do not have any documents to review'
+        <div>
+          {docs.length > 0 ? (
+            <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>
+                    <Text weight="bold">From</Text>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {docs.map(doc => (
+                  <Table.Row key={doc.docRef}>
+                    <Table.Cell>
+                      <Text>{doc.emails}</Text>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        onClick={event => {
+                          const { docRef, docId } = doc;
+                          dispatch(setDocToView({ docRef, docId }));
+                          navigate(`/viewDocument`);
+                        }}
+                        text="View"
+                        color="blue"
+                        inline
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          ) : (
+            'You do not have any documents to review'
+          )}
+        </div>
       )}
     </div>
   );
