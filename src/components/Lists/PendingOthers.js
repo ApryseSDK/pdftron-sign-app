366b2c92f9da367ae94e12228f51504c1b0e6166
Correy Lim <correylim@gmail.com>
2021-10-08 16:05:13 -0700

    Retrieve pending documents awaiting others

--- src/components/Lists/PendingOthers.js
+++ src/components/Lists/PendingOthers.js
@@ -2,22 +2,28 @@ import React, {
   useEffect,
   useState,
 } from 'react';
-import { useSelector } from 'react-redux';
+import {
+  Button,
+  Table,
+  Text,
+} from 'gestalt';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from '../../firebase/firebaseSlice';
+import { setDocToView } from '../ViewDocument/ViewDocumentSlice';
 import { searchForWaitingOnOthersDocuments } from '../../firebase/firebase';
+import { navigate } from '@reach/router';
 
 const PendingOthers = () => {
   const user = useSelector(selectUser);
   const { email } = user;
   const [docsWaitingOnOthers, setDocsWaitingOnOthers] = useState([]);
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
     async function getDocs() {
-      /**
-       * @todo @andrey Fix query for searchForWaitingOnOthersDocuments
-       */
-      // const waitingOnOthers = await searchForWaitingOnOthersDocuments(email);
-      // setDocsWaitingOnOthers(waitingOnOthers);
+      const waitingOnOthers = await searchForWaitingOnOthersDocuments(email);
+      setDocsWaitingOnOthers(waitingOnOthers);
     }
     setTimeout(getDocs, 1000);
   }, [email]);
@@ -27,6 +33,58 @@ const PendingOthers = () => {
       {
         !docsWaitingOnOthers.length
         && 'There are no documents pending other signers'
+      }
+      {
+        docsWaitingOnOthers.length
+        && (
+          <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>
+                    <Text weight="bold">Signed By</Text>
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <Text weight="bold">Awaiting Signatures</Text>
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <Text weight="bold">Last Activity</Text>
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {docsWaitingOnOthers.map(doc => (
+                  <Table.Row key={doc.docRef}>
+                    <Table.Cell>
+                      {doc.emails.filter(email => !doc.remainingToSign.includes(email)).map(email => (
+                        <Text key={email}>{email}</Text>
+                      ))}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {
+                        doc.remainingToSign.map(remainingEmail => (
+                          <Text>{remainingEmail}</Text>
+                        ))
+                      }
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        onClick={event => {
+                          const { docRef, docId } = doc;
+                          dispatch(setDocToView({ docRef, docId }));
+                          navigate(`/viewDocument`);
+                        }}
+                        text="View"
+                        color="blue"
+                        inline
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+        )
       }
     </div>
   );
