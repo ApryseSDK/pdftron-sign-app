28421308bcf3144101848c65b0941c0800af97b9
Correy Lim <correylim@gmail.com>
2021-10-08 14:40:08 -0700

    Add functionality to Tab switching
    Render the relevant components for Requestors and Signers based on which
    tab is active

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -73,92 +73,105 @@ const ProfilePage = () => {
   return (
     <div>
       <Profile />
-      <Container>
         <ModeSwitchingTabs/>
-        <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
-          <Box padding={3} color={GestaltBoxHeaderColor}>
-            <Flex alignItems="center" gap={2}>
-              <Heading size="md">
-                {`Sign Documents`}
-              </Heading>
-              {
-                /**
-                 * @todo 2021-10-08
-                 * 1. Find a better component compared to `Tag` for displaying the
-                 * number?
-                 * 2. @andrey Need API to conditionally render the number in the
-                 * `Tag` and the correct Status
-                 */
-              }
-              <Tag disabled text={`${numPendingDocsToSign}`}></Tag>
-              {
-                (!numPendingDocsToSign && <Status type="ok" title="No Pending Documents"/>)
-                || (numPendingDocsToSign && <Status type="inProgress" title="Pending Documents"/>)
-              }
-            </Flex>
-          </Box>
-          <Box padding={3} color={GestaltBodyHeaderColor}>
-            <SignList />
-          </Box>
-        </Box>
-        <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
-          <Box padding={3} color={GestaltBoxHeaderColor}>
-            <Heading size="md">{`Prepare Document`}</Heading>
-          </Box>
-          <Box padding={2} color={GestaltBodyHeaderColor}>
-            <Button
-              onClick={event => {
-                navigate(`/assignUsers`);
-              }}
-              text="Prepare Document for Signing"
-              color="blue"
-              inline
-            />
-          </Box>
-        </Box>
-        <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
-          <Box padding={3} color={GestaltBoxHeaderColor}>
-            <Flex alignItems="center" gap={2}>
-              <Heading size="md">Pending Other Signers</Heading>
-              {
-                /**
-                 * @todo 2021-10-08
-                 * 1. Find a better component compared to `Tag` for displaying the
-                 * number?
-                 */
-              }
-              <Tag disabled text={`${numPendingDocsToSignFromOthers}`}></Tag>
-            </Flex>
-            {
-              /**
-               * @todo 2021-10-08
-               * Refactor Lists/SignedList component so we can re-use it for this?
-               */
-            }
-          </Box>
-          <Box padding={3} color={GestaltBodyHeaderColor}>
-            There are no documents pending other signers
-          </Box>
-        </Box>
-        <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
-          <Box padding={3} color={GestaltBoxHeaderColor}>
-            <Flex alignItems="center" gap={2}>
-              <Heading size="md">{`Review Signed Documents`}</Heading>
-              {
-                /**
-                 * @todo 2021-10-08
-                 * 1. Find a better component compared to `Tag` for displaying the
-                 * number?
-                 */
-              }
-              <Tag disabled text={`${numOfDocsSigned}`}></Tag>
-            </Flex>
-          </Box>
-          <Box padding={3} color={GestaltBodyHeaderColor}>
-            <SignedList />
-          </Box>
-        </Box>
-      </Container>
+        {
+          // Request
+          activeTabIndex === 0
+          && (
+            <Container>
+              <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
+                <Box padding={3} color={GestaltBoxHeaderColor}>
+                  <Heading size="md">{`Prepare Document`}</Heading>
+                </Box>
+                <Box padding={2} color={GestaltBodyHeaderColor}>
+                  <Button
+                    onClick={event => {
+                      navigate(`/assignUsers`);
+                    }}
+                    text="Prepare Document for Signing"
+                    color="blue"
+                    inline
+                  />
+                </Box>
+              </Box>
+            </Container>
+          )
+        }
+        {
+          activeTabIndex === 1
+          && (
+            <Container>
+              <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
+                <Box padding={3} color={GestaltBoxHeaderColor}>
+                  <Flex alignItems="center" gap={2}>
+                    <Heading size="md">
+                      {`Sign Documents`}
+                    </Heading>
+                    {
+                      /**
+                       * @todo 2021-10-08
+                       * 1. Find a better component compared to `Tag` for displaying the
+                       * number?
+                       * 2. @andrey Need API to conditionally render the number in the
+                       * `Tag` and the correct Status
+                       */
+                    }
+                    <Tag disabled text={`${numPendingDocsToSign}`}></Tag>
+                    {
+                      (!numPendingDocsToSign && <Status type="ok" title="No Pending Documents"/>)
+                      || (numPendingDocsToSign && <Status type="inProgress" title="Pending Documents"/>)
+                    }
+                  </Flex>
+                </Box>
+                <Box padding={3} color={GestaltBodyHeaderColor}>
+                  <SignList />
+                </Box>
+              </Box>
+              <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
+                <Box padding={3} color={GestaltBoxHeaderColor}>
+                  <Flex alignItems="center" gap={2}>
+                    <Heading size="md">Pending Other Signers</Heading>
+                    {
+                      /**
+                       * @todo 2021-10-08
+                       * 1. Find a better component compared to `Tag` for displaying the
+                       * number?
+                       */
+                    }
+                    <Tag disabled text={`${numPendingDocsToSignFromOthers}`}></Tag>
+                  </Flex>
+                  {
+                    /**
+                     * @todo 2021-10-08
+                     * Refactor Lists/SignedList component so we can re-use it for this?
+                     */
+                  }
+                </Box>
+                <Box padding={3} color={GestaltBodyHeaderColor}>
+                  There are no documents pending other signers
+                </Box>
+              </Box>
+              <Box marginTop={MarginBetweenBoxes} marginBottom={MarginBetweenBoxes}>
+                <Box padding={3} color={GestaltBoxHeaderColor}>
+                  <Flex alignItems="center" gap={2}>
+                    <Heading size="md">{`Review Signed Documents`}</Heading>
+                    {
+                      /**
+                       * @todo 2021-10-08
+                       * 1. Find a better component compared to `Tag` for displaying the
+                       * number?
+                       */
+                    }
+                    <Tag disabled text={`${numOfDocsSigned}`}></Tag>
+                  </Flex>
+                </Box>
+                <Box padding={3} color={GestaltBodyHeaderColor}>
+                  <SignedList />
+                </Box>
+              </Box>
+            </Container>
+          )
+        }
     </div>
   );
 };
