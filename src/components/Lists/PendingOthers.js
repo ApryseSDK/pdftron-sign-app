59a80b73d3a54a2ae482f3bfe25fb4d43d135892
Correy Lim <correylim@gmail.com>
2021-10-08 15:08:42 -0700

    Add state for docs waiting on others
    In future commit, use state to conditionally render message or table
    based on results

--- src/components/Lists/PendingOthers.js
+++ src/components/Lists/PendingOthers.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../firebase/firebaseSlice';
+import { searchForWaitingOnOthersDocuments } from '../../firebase/firebase';
 
 const PendingOthers = () => {
+  const user = useSelector(selectUser);
+  const { email } = user;
+  const [docsWaitingOnOthers, setDocsWaitingOnOthers] = useState([]);
+
+  useEffect(() => {
+    async function getDocs() {
+      /**
+       * @todo @andrey Fix query for searchForWaitingOnOthersDocuments
+       */
+      // const waitingOnOthers = await searchForWaitingOnOthersDocuments(email);
+      // setDocsWaitingOnOthers(waitingOnOthers);
+    }
+    setTimeout(getDocs, 1000);
+  }, [email]);
+
   return (
     <div>
-      There are no documents pending other signers
+      {
+        !docsWaitingOnOthers.length
+        && 'There are no documents pending other signers'
+      }
     </div>
   );
 };
